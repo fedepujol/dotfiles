f20723a6600bf848977c83b76a941066d85373b6
fedepujol <federicodpujol@gmail.com>
2021-04-16 19:38:15 -0300

    Agrego configuracion de los pluggins

---
+++ .config/nvim/vim-plug/settings/nvimr.lua
@@ -0,0 +1,5 @@
+-- Ranger Settings
+vim.g.rnvimr_ex_enable = 1          -- Make ranger replace NetWR
+vim.g.rnvimr_draw_border = 1        -- Disable border for floating window
+vim.g.rnvimr_enable_bw = 1          -- Make NeoVim delete the buffers corresponding to the files deleted by Ranger
+vim.g.rnvimr_enable_picker = 1      -- Make Ranger to be hidden after pick a file
