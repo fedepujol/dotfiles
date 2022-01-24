e9a28b00934bc517cca8a079fe65fcfccb36101c
fedepujol <9yjbd325@anonaddy.me>
2022-01-24 11:17:24 -0300

    Update dmenu config

--- dmenu/dmenu.sh
+++ dmenu/dmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dmenu_run -l 20 -c -p 'Search' -fn 'Ubuntu Nerd Font:style=Bold:pixelsize=10:antialias=True' -nb '#161819' -sb '#60C1BC' -nf '#c5c8c6' -sf '#464688'
+dmenu_run -h 18 -p '>' -fn 'Ubuntu Nerd Font:style=Bold:pixelsize=10:antialias=True' -nb '#1E1E1E' -nf '#c5c8c6' 
